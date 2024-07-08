e58de29f4e667a2035b06312388348ed627e073e
UsmanI2506 <usiismailjee@gmail.com>
2024-07-08 16:48:55 +0100

    updated readme and code formatting

--- project_sql/5_optimal_skills.sql
+++ project_sql/5_optimal_skills.sql
@@ -15,8 +15,8 @@ WITH skills_demand AS (
     INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
     INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
     WHERE
-            job_title_short = 'Data Analyst' AND
-            salary_year_avg IS NOT NULL
+            job_title_short = 'Data Analyst'
+            AND salary_year_avg IS NOT NULL
     GROUP BY
             skills_dim.skill_id
 ),
@@ -29,8 +29,8 @@ average_salary AS (
     INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
     INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
     WHERE
-            job_title_short = 'Data Analyst' AND
-            salary_year_avg IS NOT NULL
+            job_title_short = 'Data Analyst'
+            AND salary_year_avg IS NOT NULL
     GROUP BY
             skills_job_dim.skill_id
 )
